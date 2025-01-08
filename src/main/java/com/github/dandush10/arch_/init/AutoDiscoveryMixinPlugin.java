943ec361fea13a7b9cb2e57cd228468bb64a38e9
GitHub Action <action@github.com>
2025-01-08 20:45:28 +0000

    Initialize template repository proper
    Set package name to com.github.dandush10.arch_
    Set project name to arch_
    Set modid to arch

--- src/main/java/com/github/dandush10/arch_/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/dandush10/arch_/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.dandush10.arch_.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/dandush10/arch_/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/dandush10/arch_/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.dandush10.arch_.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
